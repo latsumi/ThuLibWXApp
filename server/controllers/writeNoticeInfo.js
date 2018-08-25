1a79d4a3a4abc99e7cdfb107df4d8ba408e243a9
Jingxiao <xfeifuture@gmail.com>
2018-08-25 10:57:01 +0800

    编辑公告

--- server/controllers/writeNoticeInfo.js
+++ server/controllers/writeNoticeInfo.js
@@ -2,23 +2,22 @@
 module.exports = async ctx  => {
 //	if (ctx.state.$wxInfo.loginState === 1) {
 		const { mysql } = require('../qcloud')
-		if (ctx.method === 'GET'){
-			const query = ctx.query
-			let res = await mysql('Notice_Info').select('*').orderBy('top', 'desc')
-			let toppest = res[0].top
-			let toppest_id = res[0].id
-			await mysql('Notice_Info').where({ id: toppest_id }).update({ top: toppest + 1 })
-			await mysql('Notice_Info').insert({ title: query.title, detail: query.detail, top: toppest })
-			ctx.state.data = { toppest, toppest_id }
-		}
 		if (ctx.method === 'POST'){
 			const query = ctx.request.body
-			let res = await mysql('Notice_Info').select('*').orderBy('top','desc')
-			let toppest = res[0].top
-			let toppest_id = res[0].id
-			await mysql('Notice_Info').where({id: toppest_id}).update({top: toppest+1})
-			await mysql('Notice_Info').insert({ title: query.title, detail: query.detail, top: toppest })
-			ctx.state.data = { toppest, toppest_id }
+			if(res = await mysql('Notice_Info').select('*').orderBy('top','desc')){
+        let toppest = res[0].top
+        let toppest_id = res[0].id
+        await mysql('Notice_Info').where({id: toppest_id}).update({top: toppest+1})
+        await mysql('Notice_Info').insert({ title: query.title, detail: query.detail, top: toppest })
+        ctx.state.data = { toppest, toppest_id }
+      }
+      else{
+        toppest = 1
+        await mysql('Notice_Info').insert({ title: query.title, detail: query.detail, top: toppest })
+        res = await mysql('Notice_Info').select('*').orderBy('top', 'desc')
+        let toppest_id = res[0].id
+        ctx.state.data = { toppest, toppest_id }
+      }
 		}
 		
 		
