adf33ef419abbcb63e9498430974934ac1b039b8
曹思奇 <375136282@qq.com>
2018-08-27 02:35:08 +0800

    dutyForm: optimize some function
    modified api about dutyform

--- client/pages/toWork/editDutyForm/publishDutyForm.js
+++ client/pages/toWork/editDutyForm/publishDutyForm.js
@@ -6,6 +6,10 @@ Page({
   /**
    * 页面的初始数据
    */
+  hasChoose: '',
+  listData0: [],  //社科的列表
+  listData1: [],  //科技的列表
+  library: 0,
   data: {
     radioLibrary: [
       { name: '社科', value: '0' },
@@ -22,6 +26,21 @@ Page({
     })
   },
 
+  bindRadioChange: function (e) {
+    console.log("选择项的value是： ", e.detail.value)
+    this.setData({
+      library: e.detail.value,
+    })
+  },
+  bindPickerChange: function (e) {
+    var id = e.detail.value;
+    var data = (library == 0) ? this.data.listData0 : this.data.listData1
+    var hasChoose = data[id].title;
+    console.log('现在选择的是：' + hasChoose)
+    this.setData({
+      hasChoose: hasChoose
+    })
+  },
   save: function (e) {
     var data = e.detail.value;
     console.log('提交的数据是：', data)
@@ -67,7 +86,32 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    var that = this;
+    //获取所有初版排班表列表
+    http.POST({
+      url: '', //待填
+      data: { isOrigin: true },
+      success: function (res) {
+        var listData0 = res.data.data 
+        var listData1 = []
+        //将排班表列表按库区组装
+        for (var i = 0; i < listData0.length; i++) {
+          if (!listData0[i].library == 0) {
+            listData1.push(listData0.splice(i, 1))
+            i--
+          }
+        }
+        console.log('社科的列表为：', listData0)
+        console.log('科技的列表为：', listData0)
+        that.setData({
+          listData0: listData0,
+          listData1: listData1,
+        })
+      },
+      fail: function (res) {
+        util.showFail('网络错误', '请稍后再试')
+      }, complete: function (res) { },
+    })
   },
 
   /**
