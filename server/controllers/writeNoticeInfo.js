80983a2a1f54d16c6ec0fc396a9cfee22d1fdec5
Jingxiao <xfeifuture@gmail.com>
2018-09-13 12:16:15 +0800

    repaire write Notice

--- server/controllers/writeNoticeInfo.js
+++ server/controllers/writeNoticeInfo.js
@@ -4,20 +4,21 @@ module.exports = async ctx  => {
 		const { mysql } = require('../qcloud')
 		if (ctx.method === 'POST'){
 			const query = ctx.request.body
-			if(res = await mysql('Notice_Info').select('*').orderBy('top','desc')){
-        let toppest = res[0].top
-        let toppest_id = res[0].id
-        await mysql('Notice_Info').where({id: toppest_id}).update({top: toppest+1})
-        await mysql('Notice_Info').insert({ title: query.title, detail: query.detail, top: toppest })
-        ctx.state.data = { toppest, toppest_id }
-      }
-      else{
-        toppest = 1
-        await mysql('Notice_Info').insert({ title: query.title, detail: query.detail, top: toppest })
-        res = await mysql('Notice_Info').select('*').orderBy('top', 'desc')
-        let toppest_id = res[0].id
-        ctx.state.data = { toppest, toppest_id }
-      }
+			let res = await mysql('Notice_Info').select('*').orderBy('top','desc')
+			if(res.length != 0){
+	        let toppest = res[0].top
+	        let toppest_id = res[0].id
+	        await mysql('Notice_Info').where({id: toppest_id}).update({top: toppest+1})
+	        await mysql('Notice_Info').insert({ title: query.title, detail: query.detail, top: toppest })
+	        ctx.state.data = { toppest, toppest_id }
+	      }
+	      else{
+	        toppest = 1
+	        await mysql('Notice_Info').insert({ title: query.title, detail: query.detail, top: toppest })
+	        let res = await mysql('Notice_Info').select('*').orderBy('top', 'desc')
+	        let toppest_id = res[0].id
+	        ctx.state.data = { toppest, toppest_id }
+	      }
 		}
 		
 		
