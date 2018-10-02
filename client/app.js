f6a54864cd84846022a5d5ce11967512aea3998c
曹思奇 <375136282@qq.com>
2018-10-02 16:54:26 +0800

    updateManager:autoUpdate while boot

--- client/app.js
+++ client/app.js
@@ -8,9 +8,36 @@ App({
     library: '',
     authority: 0,
     userInfo: [],
+    logged: false,
   },
 
   onLaunch: function () {
     qcloud.setLoginUrl(config.service.loginUrl)
+    //调用版本更新管理器，更新版本
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () {
+      // 新的版本下载失败
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本下载失败',
+        showCancel: false
+      })
+    })
   }
 })
